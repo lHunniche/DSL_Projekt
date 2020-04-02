66d1f4641b136a1a780d7c900c844419c6470ac7
Lasse Hunniche Klevang <lasse96-06@hotmail.com>
2020-04-02 12:02:46 +0200

    Update dsl_iot_server.py

--- dk.klevang.iotdsl/expected_code/dsl_iot_server.py
+++ dk.klevang.iotdsl/expected_code/dsl_iot_server.py
@@ -1,26 +1,29 @@
-  
 '''
 THIS IS AN EXAMPLE WEBSERVER THAT WILL BE GENERATED (WS1 IN THIS CASE)
 '''
 from flask import Flask, request, jsonify, make_response
 app = Flask(__name__)
 
+
 @app.route("/tempdata", methods=["POST"])
 def tempdata():
-  body = response.get_json()
-  temp = body.get("temp")
-  _file = open("tempdata.csv", "a")
-  _file.write(temp + "\n")
-  _file.close()
-  return "Submitted " + temp + " to file. "
+    body = request.get_json()
+    temp = body.get("temp")
+    _file = open("tempdata.csv", "a")
+    _file.write(temp + "\n")
+    _file.close()
+    return "Submitted " + temp + " to file. "
+
 
 @app.route("/lightdata", methods=["POST"])
 def lightdata():
-  body = response.get_json()
-  light = body.get("light")
-  _file = open("lightdata.csv", "a")
-  _file.wirte(light + "\n")
-  _file.close()
-  return "Submitted " + light + " to file."
-  pass
+    body = request.get_json()
+    light = body.get("light")
+    _file = open("lightdata.csv", "a")
+    _file.write(light + "\n")
+    _file.close()
+    return "Submitted " + light + " to file."
 
+print(__name__)
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8081, threaded=True)  # 19409
